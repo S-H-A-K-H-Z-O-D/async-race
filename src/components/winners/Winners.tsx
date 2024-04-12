f84c59dc31a8eb5f76aeaa19f6d6ef235289dab1
S-H-A-K-H-Z-O-D <m.shakhzod2002@gmail.com>
2024-04-12 13:40:32 +0500

    commit

--- src/components/winners/Winners.tsx
+++ src/components/winners/Winners.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import React, {useState} from 'react';
 import Pagination from "../common/Pagination.tsx";
 import {useGetSingleWinnerQuery, useGetWinnersQuery} from "../../api/api.ts";
